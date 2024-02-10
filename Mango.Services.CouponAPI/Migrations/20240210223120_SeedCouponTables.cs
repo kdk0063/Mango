2ccbb594d80cd041ba78c1f15ec6abf09f11f275
andrew kim <kdk0063@gmail.com>
2024-02-10 18:20:32 -0500

    first commit

---
+++ Mango.Services.CouponAPI/Migrations/20240210223120_SeedCouponTables.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango.Services.CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCouponTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "CouponId", "CouponCode", "DiscountAmount", "MinAmount" },
+                values: new object[] { 2, "20FF", 20.0, 20 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 2);
+        }
+    }
+}
