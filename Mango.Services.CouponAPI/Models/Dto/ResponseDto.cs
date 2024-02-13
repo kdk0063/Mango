c26741fdffbc22a9fcccc4fed7341a4904c369b3
andrew kim <kdk0063@gmail.com>
2024-02-12 22:40:56 -0500

    update CRUD for coupon page

--- Mango.Services.CouponAPI/Models/Dto/ResponseDto.cs
+++ Mango.Services.CouponAPI/Models/Dto/ResponseDto.cs
@@ -3,12 +3,7 @@
     public class ResponseDto
     {
         public object? Result { get; set; }
-<<<<<<< HEAD
-        public bool IsSuccess { get; set; }
-        public string Message { get; set; } 
-=======
         public bool IsSuccess { get; set; } = true;
         public string Message { get; set; } = "";
->>>>>>> e94211d (first commit)
     }
 }
--- Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
+++ Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ccbb594d80cd041ba78c1f15ec6abf09f11f275")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b4f0fba57e5fc041134e7d3dd4e7d0ecd83ff13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
