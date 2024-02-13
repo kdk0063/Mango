f7afdec784a0411c10d861acc074e482f80a978f
andrew kim <kdk0063@gmail.com>
2024-02-12 20:30:28 -0500

    updated git repo

--- Mango.Services.CouponAPI/Models/Dto/ResponseDto.cs
+++ Mango.Services.CouponAPI/Models/Dto/ResponseDto.cs
@@ -3,7 +3,12 @@
     public class ResponseDto
     {
         public object? Result { get; set; }
+<<<<<<< HEAD
         public bool IsSuccess { get; set; }
         public string Message { get; set; } 
+=======
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = "";
+>>>>>>> e94211d (first commit)
     }
 }
